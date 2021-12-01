849c428b5027b1e883371cf5e449377a4af520ab
Christa Hartsock <christa.hartsock@gsa.gov>
2021-12-01 11:27:52 -0800

    Use IAL2 attributes
    Co-authored-by: Ryan Ahearn <ryan.ahearn@gsa.gov>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ provider :login_dot_gov, {
     name: :logindotgov,
     client_id: 'urn:gov:gsa:openidconnect.profiles:sp:sso:tts_future:cloud_login_iaml2_test', # same value as registered in the Partner Dashboard
     idp_base_url: 'https://idp.int.identitysandbox.gov/', # login.gov sandbox environment IdP
-    ial: 1,
+    ial: 2,
     private_key: OpenSSL::PKey::RSA.new(Rails.application.credentials.login_pem),
     redirect_uri: 'http://localhost:3000/auth/logindotgov/callback',
     scope: 'openid address email phone profile'
